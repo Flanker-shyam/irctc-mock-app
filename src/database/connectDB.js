af2f764ed6e80ff08fe4103ea9758eb41783cc3f
Flanker-shyam <shyamp665@gmail.com>
2024-04-08 10:08:08 +0530

    remove extra comments

--- src/database/connectDB.js
+++ src/database/connectDB.js
@@ -1,17 +1,11 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const createUserTable = require("../schemas/userSchema");
-const createBookingTable = require("../schemas/trainSchema");
-const createTrainTable = require("../schemas/bookingSchema");
 const pool = require("./createPool");
 
 const connectDB = async () => {
     const client = null;
   try {
-    const client = await pool.connect();
-    createUserTable(client);
-    createBookingTable(client);
-    createTrainTable(client);
+    await pool.connect();
     console.log("Connected successfully to the PostgreSQL database");
   } catch (err) {
     console.error(`Error connecting to the PostgreSQL database: ${err}`);
